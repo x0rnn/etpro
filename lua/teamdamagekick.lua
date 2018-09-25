b619f8fccb57eafa0026248f3a1c62a2a9f5315e
x0rnn <c0rnn@live.com>
2018-09-26 00:42:35 +0200

    Update teamdamagekick.lua

--- lua/teamdamagekick.lua
+++ lua/teamdamagekick.lua
@@ -1,6 +1,6 @@
 checkInterval = 180000 -- interval in milliseconds to check team damage (3 minutes)
 kickReason = "You made too much team damage."
-banTime = 1800 -- ban time in seconds (30 minutes)
+banTime = 900 -- ban time in seconds (15 minutes)
 
 function et_InitGame(levelTime, randomSeed, restart)
 	et.RegisterModname("teamdamagekick.lua "..et.FindSelf())
@@ -12,7 +12,10 @@ function et_RunFrame(levelTime)
 		local team = tonumber(et.gentity_get(i, "sess.sessionTeam"))
 		if team == 1 or team == 2 then
 			local teamdamage = et.gentity_get(i, "sess.team_damage")
-			if teamdamage == 1200 then
+			if teamdamage > 800 then
+				et.trap_SendServerCommand(i, "chat \"^1You're making too much team damage. Careful or you'll get kicked.\"\n")
+			end
+			if teamdamage > 1200 then
 				et.trap_DropClient(i, kickReason, banTime)
 			end
 		end
