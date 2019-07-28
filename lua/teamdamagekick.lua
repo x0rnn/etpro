bb4a5b62d01b78813f370e1f6f8051e9e62af70e
x0rnn <c0rnn@live.com>
2019-07-28 13:39:25 +0200

    Update teamdamagekick.lua

--- lua/teamdamagekick.lua
+++ lua/teamdamagekick.lua
@@ -8,15 +8,18 @@ end
 
 function et_RunFrame(levelTime)
 	if math.mod(levelTime,checkInterval) ~= 0 then return end
-	for i=0,tonumber(et.trap_Cvar_Get("sv_maxclients"))-1 do
-		local team = tonumber(et.gentity_get(i, "sess.sessionTeam"))
-		if team == 1 or team == 2 then
-			local teamdamage = et.gentity_get(i, "sess.team_damage")
-			if teamdamage > 800 then
-				et.trap_SendServerCommand(i, "chat \"^1You're making too much team damage. Careful or you'll get kicked.\"\n")
-			end
-			if teamdamage > 1200 then
-				et.trap_DropClient(i, kickReason, banTime)
+	gamestate = tonumber(et.trap_Cvar_Get("gamestate"))
+	if gamestate == 0 then
+		for i=0,tonumber(et.trap_Cvar_Get("sv_maxclients"))-1 do
+			local team = tonumber(et.gentity_get(i, "sess.sessionTeam"))
+			if team == 1 or team == 2 then
+				local teamdamage = et.gentity_get(i, "sess.team_damage")
+				if teamdamage > 800 then
+					et.trap_SendServerCommand(i, "chat \"^1You're making too much team damage. Careful or you'll get kicked.\"\n")
+				end
+				if teamdamage > 1200 then
+					et.trap_DropClient(i, kickReason, banTime)
+				end
 			end
 		end
 	end
