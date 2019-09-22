4e18e955c3b6a21ccdf9edb09973612d32f39eca
x0rnn <c0rnn@live.com>
2019-09-22 11:06:31 +0200

    Add files via upload

--- weaponstats.py
+++ weaponstats.py
@@ -6,7 +6,7 @@
 import glob
 import re
 from collections import defaultdict
-logs = glob.glob('etserver*.log')
+logs = glob.glob('etserver*.log') # change to your log name/s
 
 players = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 m = [0, 1, 2, 4, 8, 16, 32 , 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152]
