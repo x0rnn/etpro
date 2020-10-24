8d812fd9e99031d779d6838d2774c0c625b1ecfa
x0rnn <c0rnn@live.com>
2020-10-24 10:24:45 +0200

    Add files via upload

--- lua/lamers.lua
+++ lua/lamers.lua
@@ -2,6 +2,7 @@
 -- x% chance (default 10%) to gib panzer/mortar/arty/mg42 lamers on every 5th kill who have more than min_kill (default 25) kills and min_percent (default 85%) or more of all their kills are by panzer/mortar/arty/mg42
 -- players who continuously push other players get a warning first and get put to spectators if they continue (default: single player 7x warn, 10x spec; multiple players 10x warn, 15x warn, 22x put spec). Pushing up to 10 sec after spawn doesn't count due to possible blockers, etc.
 -- players who continuously walk into other players' artillery (teamkill) will get a warning first and get put to spectators if they continue (default: warn at 6 and 7, put spec at 8)
+-- players who continuously teamkill (knife, pistols, smg (for medics only knife counts)) other players will get a warning first and get put to spectators if they continue (default: warn at 2 and 3, put spec at 4)
 -- players who just hand out ammo and do nothing else will have their ammo packs taken away until they get more kills (defaults: <1 kills/10 ammo given, <5/20, <10/35, <15/55)
 -- intended for players with a lame gamestyle of spamming/camping panzer/mortar/arty/mg42 and not doing anything else and overall laming by pushing and intentionally walking into (team) arty
 
@@ -9,8 +10,10 @@ panzerlamers = {}
 mortarlamers = {}
 artylamers = {}
 mglamers = {}
-chance = 10
+chance = 15
+chance_mortar = 30
 min_kills = 25
+min_kills_mortar = 20
 min_percent = 85
 
 shoves = {}
@@ -22,15 +25,20 @@ multi_shove_warn1 = 10
 multi_shove_warn2 = 15
 multi_shove_spec = 22
 
+tks = {}
+tks_warn1 = 2
+tks_warn2 = 3
+tks_spec = 4
+
 artywalkers = {}
-artywalk_warn1 = 6
-artywalk_warn2 = 7
-artywalk_spec = 8
+artywalk_warn1 = 5
+artywalk_warn2 = 6
+artywalk_spec = 7
 
 ammolamers = {}
 ammo_given = {}
 al_msg = {}
-al_minkills_1 = 1
+al_minkills_1 = 2
 al_minkills_2 = 5
 al_minkills_3 = 10
 al_minkills_4 = 15
@@ -64,6 +72,9 @@ function et_ClientDisconnect(clientNum)
 	if artywalkers[clientNum] ~= nil then
 		artywalkers[clientNum] = nil
 	end
+	if tks[clientNum] ~= nil then
+		tks[clientNum] = nil
+	end 
 	if ammo_given[clientNum] ~= nil then
 		ammo_given[clientNum] = nil
 	end
@@ -86,6 +97,7 @@ function et_Obituary(victim, killer, mod)
 			local v_teamid = et.gentity_get(victim, "sess.sessionTeam")
 			local kills = tonumber(et.gentity_get(killer, "sess.kills"))
 			local name = et.gentity_get(killer, "pers.netname")
+			local name2 = et.gentity_get(victim, "pers.netname")
 			local health = tonumber(et.gentity_get(killer, "health"))
 			
 			math.randomseed(et.trap_Milliseconds())
@@ -107,8 +119,9 @@ function et_Obituary(victim, killer, mod)
 										if health > 0 then
 											local choice = math.random(1, 100)
 											if choice <= chance then
-												msg = string.format("cpm  \"^3Oops. " .. name .. "^3's panzerfaust overheated from excessive use. It blew up in his face.\n")
+												msg = string.format("cpm \"^3Oops. " .. name .. "^3's panzerfaust overheated from excessive use. It blew up in his face.\n")
 												et.trap_SendServerCommand(-1, msg)
+												et.G_LogPrint("LUA event: Oops. " .. name .. "'s panzerfaust overheated from excessive use. It blew up in his face.\n")
 												et.G_Damage(killer, 80, 1022, 1000, 8, 34)
 												et.G_Sound(killer, soundindex)
 											end
@@ -127,14 +140,15 @@ function et_Obituary(victim, killer, mod)
 								mortarlamers[cl_guid][2] = kills
 							end
 
-							if mortarlamers[cl_guid][1] >= min_kills then
+							if mortarlamers[cl_guid][1] >= min_kills_mortar then
 								if mortarlamers[cl_guid][1] / kills >= min_percent/100 then
 									if math.mod(mortarlamers[cl_guid][1], 5) == 0 then
 										if health > 0 then
 											local choice = math.random(1, 100)
-											if choice <= chance then
-												msg = string.format("cpm  \"^3Oops. " .. name .. "^3's mortar overheated from excessive use. It blew up in his face.\n")
+											if choice <= chance_mortar then
+												msg = string.format("cpm \"^3Oops. " .. name .. "^3's mortar overheated from excessive use. It blew up in his face.\n")
 												et.trap_SendServerCommand(-1, msg)
+												et.G_LogPrint("LUA event: Oops. " .. name .. "'s mortar overheated from excessive use. It blew up in his face.\n")
 												et.G_Damage(killer, 80, 1022, 1000, 8, 34)
 												et.G_Sound(killer, soundindex)
 											end
@@ -159,7 +173,8 @@ function et_Obituary(victim, killer, mod)
 										if health > 0 then
 											local choice = math.random(1, 100)
 											if choice <= chance then
-												msg = string.format("cpm  \"^3Oops. " .. name .. "^3's MG42 overheated from excessive use. It blew up in his face.\n")
+												msg = string.format("cpm \"^3Oops. " .. name .. "^3's MG42 overheated from excessive use. It blew up in his face.\n")
+												et.G_LogPrint("LUA event: Oops. " .. name .. "'s MG42 overheated from excessive use. It blew up in his face.\n")
 												et.trap_SendServerCommand(-1, msg)
 												et.G_Damage(killer, 80, 1022, 1000, 8, 34)
 												et.G_Sound(killer, soundindex)
@@ -186,8 +201,9 @@ function et_Obituary(victim, killer, mod)
 										if health > 0 then
 											local choice = math.random(1, 100)
 											if choice <= chance then
-												msg = string.format("cpm  \"^3Huh. One of  " .. name .. "^3's airstrike grenades was actually a real grenade. It malfunctioned.\n")
+												msg = string.format("cpm \"^3Huh. One of " .. name .. "^3's airstrike grenades was actually a real grenade. It malfunctioned.\n")
 												et.trap_SendServerCommand(-1, msg)
+												et.G_LogPrint("LUA event: Huh. One of " .. name .. "'s airstrike grenades was actually a real grenade. It malfunctioned.\n")
 												et.G_Damage(killer, 80, 1022, 1000, 8, 34)
 												et.G_Sound(killer, soundindex)
 											end
@@ -209,14 +225,17 @@ function et_Obituary(victim, killer, mod)
 							end
 
 							if artywalkers[victim][killer] == artywalk_warn1 then
-								et.trap_SendServerCommand(victim, "chat \"^3You have walked into " .. et.gentity_get(killer, "pers.netname") .. "^3's arty a lot of times. This script thinks you're doing it intentionally.\"\n")
+								et.trap_SendServerCommand(victim, "chat \"^3You have walked into " .. name .. "^3's arty a lot of times. This script thinks you're doing it intentionally.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(victim, "pers.netname") .. " walked into " .. name .. "'s arty " .. artywalk_warn1 .. " times.\n")
 							end
 							if artywalkers[victim][killer] == artywalk_warn2 then
-								et.trap_SendServerCommand(victim, "chat \"^3If you continue walking into " .. et.gentity_get(killer, "pers.netname") .. "^3's arty, you will be put to spectators.\"\n")
+								et.trap_SendServerCommand(victim, "chat \"^3If you continue walking into " .. name .. "^3's arty, you will be put to spectators.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(victim, "pers.netname") .. " walked into " .. name .. "'s arty " .. artywalk_warn2 .. " times.\n")
 							end
 							if artywalkers[victim][killer] == artywalk_spec then
 								et.trap_SendConsoleCommand(et.EXEC_APPEND, "ref remove " .. victim .. "\n")
-								et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(victim, "pers.netname") .. " ^3moved to spectators for intentionally walking into " .. et.gentity_get(killer, "pers.netname") .. "^3's arty too many times.\"\n")
+								et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(victim, "pers.netname") .. " ^3moved to spectators for intentionally walking into " .. name .. "^3's arty too many times.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(victim, "pers.netname") .. " walked into " .. name .. "'s arty " .. artywalk_spec .. " times. Moved to spec for intentionally walking into arty.\n")
 							end
 						end
 					end
@@ -258,6 +277,67 @@ function et_Obituary(victim, killer, mod)
 							end
 						end
 					end
+				else
+					if mod == 24 then
+						msg = string.format("chat \"" .. name .. " ^3gibbed for pushing " .. et.gentity_get(victim, "pers.netname") .. "^3 into his death.\n")
+						et.G_LogPrint("LUA event: " .. name .. " gibbed for pushing " .. et.gentity_get(victim, "pers.netname") .. " into his death.\n")
+						et.trap_SendServerCommand(-1, msg)
+						et.G_Damage(killer, 80, 1022, 1000, 8, 34)
+						et.G_Sound(killer, et.G_SoundIndex("/sound/etpro/osp_goat.wav"))
+					end
+					if mod == 6 or mod == 7 or mod == 8 or mod == 9 or mod == 10 or mod == 58 or mod == 59 then -- knife, luger, colt, mp40, thompson, akimbo colt, akimbo luger
+						if et.gentity_get(killer,"sess.PlayerType") ~= 1 then -- medic
+							if tks[killer] == nil then
+								tks[killer] = { [victim] = 1 }
+							else
+								if tks[killer][victim] == nil then
+									tks[killer][victim] = 1
+								else
+									tks[killer][victim] = tks[killer][victim] + 1
+								end
+
+								if tks[killer][victim] == tks_warn1 then
+									et.trap_SendServerCommand(killer, "chat \"^3You teamkilled " .. name2 .. "^3" .. tks_warn1 .. " times. This script thinks you're doing it intentionally.\"\n")
+									et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. " " .. tks_warn1 .. " times.\n")
+								end
+								if tks[killer][victim] == tks_warn2 then
+									et.trap_SendServerCommand(killer, "chat \"^3If you continue teamkilling " .. name2 .. "^3, you will be put to spectators.\"\n")
+									et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. " " .. tks_warn2 .. " times.\n")
+								end
+								if tks[killer][victim] == tks_spec then
+									et.trap_SendConsoleCommand(et.EXEC_APPEND, "ref remove " .. killer .. "\n")
+									et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(killer, "pers.netname") .. " ^3moved to spectators for intentionally teamkilling " .. name2 .. "^3 too many times.\"\n")
+									et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. "' " .. tks_spec .. " times. Moved to spec for intentionally teamkilling.\n")
+								end
+							end
+						else
+							if mod == 6 then -- knife
+								if tks[killer] == nil then
+									tks[killer] = { [victim] = 1 }
+								else
+									if tks[killer][victim] == nil then
+										tks[killer][victim] = 1
+									else
+										tks[killer][victim] = tks[killer][victim] + 1
+									end
+
+									if tks[killer][victim] == tks_warn1 then
+										et.trap_SendServerCommand(killer, "chat \"^3You teamkilled " .. name2 .. "^3" .. tks_warn1 .. " times. This script thinks you're doing it intentionally.\"\n")
+										et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. " " .. tks_warn1 .. " times.\n")
+									end
+									if tks[killer][victim] == tks_warn2 then
+										et.trap_SendServerCommand(killer, "chat \"^3If you continue teamkilling " .. name2 .. "^3, you will be put to spectators.\"\n")
+										et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. " " .. tks_warn2 .. " times.\n")
+									end
+									if tks[killer][victim] == tks_spec then
+										et.trap_SendConsoleCommand(et.EXEC_APPEND, "ref remove " .. killer .. "\n")
+										et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(killer, "pers.netname") .. " ^3moved to spectators for intentionally teamkilling " .. name2 .. "^3 too many times.\"\n")
+										et.G_LogPrint("LUA event: " .. et.gentity_get(killer, "pers.netname") .. " teamkilled " .. name2 .. "' " .. tks_spec .. " times. Moved to spec for intentionally teamkilling.\n")
+									end
+								end
+							end
+						end
+					end
 				end
 			end
 		end
@@ -285,10 +365,12 @@ function et_Print(text)
 
 							if shoves[tonumber(id1)][tonumber(id2)] == single_shove_warn then
 								et.trap_SendServerCommand(tonumber(id1), "chat \"^3You have shoved " .. et.gentity_get(tonumber(id2), "pers.netname") .. " ^3a lot of times. If you continue, you will be put to spectators.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(tonumber(id1), "pers.netname") .. " shoved " .. et.gentity_get(tonumber(id2), "pers.netname") .. " " .. single_shove_warn .. " times.\n")
 							end
 							if shoves[tonumber(id1)][tonumber(id2)] == single_shove_spec then
 								et.trap_SendConsoleCommand(et.EXEC_APPEND, "ref remove " .. tonumber(id1) .. "\n")
 								et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(tonumber(id1), "pers.netname") .. " ^3moved to spectators for pushing " .. et.gentity_get(tonumber(id2), "pers.netname") .. "^3's buttons one too many times.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(tonumber(id1), "pers.netname") .. " shoved " .. et.gentity_get(tonumber(id2), "pers.netname") .. " " .. single_shove_spec .. " times. Moved to spec.\n")
 								shove_flag = true
 							end
 
@@ -300,13 +382,16 @@ function et_Print(text)
 							end
 							if sum == multi_shove_warn1 and i > 1 then
 								et.trap_SendServerCommand(tonumber(id1), "chat \"^3You have shoved a lot of players. Is it really necessary?\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(tonumber(id1), "pers.netname") .. " shoved a lot of players " .. multi_shove_warn1 .. " times.\n")
 							end
 							if sum == multi_shove_warn2 and i > 1 then
 								et.trap_SendServerCommand(tonumber(id1), "chat \"^3You have shoved a lot of players. If you continue, you will be put to spectators.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(tonumber(id1), "pers.netname") .. " shoved a lot of players " .. multi_shove_warn2 .. " times.\n")
 							end
 							if sum == multi_shove_spec then
 								et.trap_SendConsoleCommand(et.EXEC_APPEND, "ref remove " .. tonumber(id1) .. "\n")
 								et.trap_SendServerCommand(-1, "chat \"" .. et.gentity_get(tonumber(id1), "pers.netname") .. " ^3moved to spectators for shoving too many players too many times.\"\n")
+								et.G_LogPrint("LUA event: " .. et.gentity_get(tonumber(id1), "pers.netname") .. " shoved a lot of players " .. multi_shove_spec .. " times. Moved to spec.\n")
 								shove_flag = true
 							end
 
@@ -415,4 +500,28 @@ function et_ClientSpawn(id, revived)
 			end
 		end
 	end
+end
+
+function et_ClientCommand(id, command)
+	if et.trap_Argv(0) == "say" then
+		args = et.ConcatArgs(1)
+		local args_table = {}
+		cnt = 0
+		for i in string.gfind(args, "%S+") do
+			table.insert(args_table, i)
+			cnt = cnt + 1
+		end
+		if args_table[1] == "!admin" then
+			if cnt == 1 then
+				et.trap_SendServerCommand(id, "chat \"^3Please include a reason when you're calling an admin!\"")
+				return 1
+			elseif cnt == 2 then
+				if string.lower(args_table[2]) == "admin" then
+					et.trap_SendServerCommand(id, "chat \"^3Please include a valid reason when calling an admin.\"")
+					return 1
+				end
+			end
+		end
+	end
+	return(0)
 end
